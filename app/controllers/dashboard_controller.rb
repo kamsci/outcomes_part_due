66ab70f1a6753518f8cbed3b3441139316c33b8c
Krista Calderon <kamsci@gmail.com>
2016-09-14 11:20:52 -0700

    added ADash frame and placeholder content

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,7 @@ class DashboardController < ApplicationController
 
     @active_cohorts = (1..3).to_a
 
+    @students = ('a'..'t').to_a
+
   end
 end
--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "dashboard/index.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  xit 'renders _dashboard_cohort partial for each cohort' do
-    assign (:avtive_cohorts, [double(c), double(c), double(c)])
+  it 'renders _dashboard_cohort partial for each cohort' do
+    assign (:active_cohorts, [double(c), double(c), double(c)])
     render
 
     expect(view).to render_template(:partial => '_dashboard_cohort', :count => 3)
@@ -14,10 +14,10 @@ end
 
 ######## Test Examples for View ########
 
-RSpec.describe "events/index", :type => :view do
-  it "renders _event partial for each event" do
-    assign(:events, [double(Event), double(Event)])
-    render
-    expect(view).to render_template(:partial => "_event", :count => 2)
-  end
-end
+# RSpec.describe "events/index", :type => :view do
+#   it "renders _event partial for each event" do
+#     assign(:events, [double(Event), double(Event)])
+#     render
+#     expect(view).to render_template(:partial => "_event", :count => 2)
+#   end
+# end
