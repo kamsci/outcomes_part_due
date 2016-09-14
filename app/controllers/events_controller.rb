fc8d7eb06e954f221bf2b01a06c6a30eb9031bcd
Krista Calderon <kamsci@gmail.com>
2016-09-13 17:29:57 -0700

    added html in events; api code in ctrl, api not working yet

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,29 @@
 class EventsController < ApplicationController
   def index
+    
+    zip = '98101'
+    country = 'USA'
+    city = 'Seattle'
+
+    if !params[':topic'] || params[':topic'] == ''
+      @topic = 'Web'
+    else
+      @topic = params[':topic'].gsub(/\s+/, "")
+    end
+
+    begin
+      data = RestClient.get 'https://api.meetup.com/2/open_events?sign=true&photo-host=public&zip=' + zip + '&country=' + country + '&topic=' + @topic + '&city=' + city + '&state=WA&page=20&key=23145310778c71694fbb51774f'
+    rescue
+      flash[:danger] = 'No Search Results, please try again'
+      puts 'I am rescued'
+    end
+     #  @data = JSON.parse(data)
+
+    if data == nil
+      flash[:danger] = 'No Search Results, please try again'
+    else
+      @data = JSON.parse(data)
+    end
+
   end
 end
